ee0ac40fcc859be68e7c6249b9a972ca47377c98
Nicolas Pellegrin <npellegrin.dev@gmail.com>
2016-04-10 23:24:09 +0200

    Add world customization sample

---
+++ clusters/Cluster_SurfaceOnly_CustomWorld/Master/worldgenoverride.lua
@@ -0,0 +1,64 @@
+return {
+	override_enabled=true,
+	overrides={
+
+		-- Misc params
+		boons="default", --options are "never", "rare", "default", "often", "always"
+		branching="default", --options are "never", "least", "default", "most"
+		day="longday", --options are "onlyday", "onlydusk", "onlynight", "default", "longday", "longdusk", "longnight"
+		lightning="default", --options are "never", "rare", "default", "often", "always"
+		loop="default", --options are "never", "default", "always"
+		season="default", --options are "onlysummer", "onlywinter", "default", "longsummer", "longwinter", "longboth", "shortboth"
+		season_start="summer", --options are "winter", "summer"
+		touchstone="default", --options are "never", "rare", "default", "often", "always"
+		weather="default", --options are "never", "rare", "default", "often", "always"
+		world_size="default" --options are "default", "medium", "large", "huge"
+
+		-- Animals params
+		-- Animal options are "never", "rare", "default", "often", "always"
+		alternatehunt="default",
+		angrybees="default",
+		beefalo="often",
+		beefaloheat="default",
+		bees="default",
+		birds="often",
+		butterfly="often",
+		frogs="default",
+		hunt="default",
+		penguins="default",
+		perd="default",
+		pigs="default",
+		rabbits="often",
+		tallbirds="default"
+
+		-- Monsters params
+		-- Monsters options are "never", "rare", "default", "often", "always"
+		chess="default",
+		deerclops="default",
+		hounds="default",
+		krampus="default",
+		liefs="default",
+		lureplants="default",
+		merm="default",
+		spiders="default",
+		tentacles="default",
+		walrus="default"
+
+		-- Resources params
+		-- Resources options are "never", "rare", "default", "often", "always"
+		flint="default",
+		flowers="often",
+		grass="often",
+		marshbush="default",
+		meteorshowers="default",
+		meteorspawner="default",
+		reeds="default",
+		rock="often",
+		rocks="often",
+		sapling="default",
+		trees="default"
+		berrybush="often",
+		carrot="often",
+		mushroom="often"
+	}
+}
